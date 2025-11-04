cbc4f7bcb2a0efb9a24a962a0332a128b729fa5e
HoneyNat1 <23nacostahernandez@gmail.com>
2025-11-03 21:00:01 -0700

    Update TOP.v
    Changes made were adding the muxes and connecting everything together

--- Lab4/TOP.v
+++ Lab4/TOP.v
@@ -5,7 +5,7 @@ module top (
 
     wire [15:0] PC_value;
     wire [15:0] mem_dout; // use this in IR module as an input
-    //wire [15:0] IR_out; // IR MODULE
+    wire [15:0] data_dout; // Data output from memory
     wire        PCe;        // PC enable
     wire        Ren;        // Register write enable
     wire [3:0]  Rsrc;       // Source reg index
@@ -13,8 +13,12 @@ module top (
     wire        R_I;        // Reg/Imm select
     wire [7:0]  Opcode;     // ALU opcode
     wire [7:0]  Imm;        // Immediate value
+	 wire 		 mem_WE;		 // Memory write enable (for STORE)
     wire [15:0] alu_out;
     wire [4:0]  flags;
+
+	 wire	[1:0]	 LSCntl; 
+	 wire 		 ALU_MUX_Cntl; 
 
 	 
     pc uPC (
@@ -24,30 +28,20 @@ module top (
         .pc_value(PC_value)
     );
 
-    Bram uBram (
-        .data_a(16'h0000),    // no writeback to memory yet
-        .data_b(16'h0000),
-        .addr_a(PC_value),    // instruction address = PC
-        .addr_b(10'b0),
-        .we_a(1'b0),
-        .we_b(1'b0),
-        .clk(clk),
-        .q_a(mem_dout),
-        .q_b()                // unused for now
-    );
-
-
     controlFSM uFSM (
         .clk(clk),
         .rst(rst),
-        .inst(mem_dout),	// we need IR module
+        .inst(mem_dout),
         .PCe(PCe),
         .Ren(Ren),
         .Rsrc(Rsrc),
         .Rdest(Rdest),
         .R_I(R_I),
         .Opcode(Opcode),
-        .Imm(Imm)
+        .Imm(Imm),
+		  .mem_WE(mem_WE),
+		  .LSCntl(LSCntl),
+		  .ALU_MUX_Cntl(ALU_MUX_Cntl)
     );
 
     RegALU uRegALU (
@@ -60,8 +54,36 @@ module top (
         //.cin(1'b0),         
         .immB({8'h00, Imm}),   // zero-extend immediate
         .selB_imm(R_I),
-        .alu_out(alu_out),
+        .alu_out(alu_result),
         .flags(flags)
+    );
+
+	 assign alu_out = alu_result;
+
+    // LSCntl MUX
+    reg [15:0] mem_addr;
+    always @(*) begin
+        case (LSCntl)
+            2'b00: mem_addr = PC_value;               // instruction fetch
+            2'b01: mem_addr = {12'b0, Rdest};         // simulated reg value
+            2'b10: mem_addr = {12'b0, Rsrc};          // simulated reg value
+            default: mem_addr = 16'h0000;
+        endcase
+    end
+
+    // write back MUX
+    wire [15:0] wb_data = (ALU_MUX_Cntl) ? data_dout : alu_out;
+
+	 Bram uBram (
+        .data_a(16'h0000),    // no writeback to memory yet
+        .data_b(alu_out),		// data to store (RegB)
+        .addr_a(PC_value),    // instruction address = PC
+        .addr_b(mem_addr),
+        .we_a(1'b0),
+        .we_b(mem_WE),			// FSM controls WE
+        .clk(clk),
+        .q_a(mem_dout),
+        .q_b(data_dout)
     );
 
 endmodule
