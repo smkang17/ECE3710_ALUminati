93efeea0a57ca9a26f2e0494605f475a62bbf56c
HoneyNat1 <23nacostahernandez@gmail.com>
2025-11-11 13:04:58 -0700

    Update top.v
    updated pc and controlFSM module in top

--- Lab4/top.v
+++ Lab4/top.v
@@ -4,8 +4,7 @@ module top (
 );
 
     wire [15:0] PC_value;
-    wire [15:0] mem_dout; // use this in IR module as an input
-    wire [15:0] IR_out; // IR MODULE
+    wire [15:0] mem_dout;   // use this in IR module as an input
     wire        PCe;        // PC enable
     wire        Ren;        // Register write enable
     wire [3:0]  Rsrc;       // Source reg index
@@ -13,41 +12,67 @@ module top (
     wire        R_I;        // Reg/Imm select
     wire [7:0]  Opcode;     // ALU opcode
     wire [7:0]  Imm;        // Immediate value
+	 wire 		 mem_WE;		 // Memory write enable (for STORE)
     wire [15:0] alu_out;
     wire [4:0]  flags;
-
+	 wire [15:0] busA_out;   
+	 wire [15:0] busB_out;
+	 wire	    	 LSCntl; 
+	 wire 		 ALU_MUX_Cntl; 
+	 
+	 wire	[1:0]	 PCsrc;
+	 wire	[15:0] branch_disp;
+	 
+	 // LSCntl MUX
+    reg [15:0] mem_addr;
+    always @(LSCntl or PC_value or busB_out) begin
+        case (LSCntl)
+            1'b0: mem_addr = PC_value;               // instruction fetch
+            1'b1: mem_addr = busB_out;               // simulated reg value
+            default: mem_addr = 16'h0000;
+        endcase
+    end
+	 
+	 
+	 //ALU_MUX_Cntl MUX
+	 reg [15:0] wb_data;
+	 always @(ALU_MUX_Cntl or alu_out or mem_dout) begin
+        case (ALU_MUX_Cntl)
+            1'b0: wb_data = alu_out;               // instruction fetch
+            1'b1: wb_data = mem_dout;               // simulated reg value
+            default: wb_data = 16'h0000;
+        endcase
+    end
+	 
+	 
 	 
     pc uPC (
         .clk(clk),
         .rst(rst),
         .PCe(PCe),
+		  .PCsrc(PCsrc),
+		  .branch_disp(branch_disp),
+		  .Rtarget(busB_out),
         .pc_value(PC_value)
     );
 
-    Bram uBram (
-        .data_a(16'h0000),    // no writeback to memory yet
-        .data_b(16'h0000),
-        .addr_a(PC_value),    // instruction address = PC
-        .addr_b(10'b0),
-        .we_a(1'b0),
-        .we_b(1'b0),
-        .clk(clk),
-        .q_a(mem_dout),
-        .q_b()                // unused
-    );
-
-
-    ControlFSM uFSM (
+    controlFSM uFSM (
         .clk(clk),
         .rst(rst),
-        .IR(IR_out),	// we need IR module
+        .inst(mem_dout),
+		  .flags(flags)
         .PCe(PCe),
+		  .PCsrc(Pcsrc),
+		  .branch_disp(branch_disp),
         .Ren(Ren),
         .Rsrc(Rsrc),
         .Rdest(Rdest),
         .R_I(R_I),
         .Opcode(Opcode),
-        .Imm(Imm)
+        .Imm(Imm),
+		  .mem_WE(mem_WE),
+		  .LSCntl(LSCntl),
+		  .ALU_MUX_Cntl(ALU_MUX_Cntl)
     );
 
     RegALU uRegALU (
@@ -61,7 +86,24 @@ module top (
         .immB({8'h00, Imm}),   // zero-extend immediate
         .selB_imm(R_I),
         .alu_out(alu_out),
-        .flags(flags)
+        .flags(flags),
+		  .busA_out(busA_out),
+		  .busB_out(busB_out),
+		  .wb_data(wb_data)
+    );
+
+	 
+	 //b not used yet
+	 Bram uBram (
+        .data_a(busA_out),   	// data stored from Rsrc during store inst
+        .data_b(16'h0000),		
+        .addr_a(mem_addr),    // instruction address = PC
+        .addr_b(1'b0),
+        .we_a(mem_WE),
+        .we_b(1'b0),			// FSM controls WE
+        .clk(clk),
+        .q_a(mem_dout),
+        .q_b()
     );
 
 endmodule
