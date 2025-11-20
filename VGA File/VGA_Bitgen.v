9d18f18889d72cae3598235d53574bc11dcae753
HoneyNat1 <23nacostahernandez@gmail.com>
2025-11-19 17:22:16 -0700

    Update VGA_Bitgen.v
    direct graphics approach
    a simple red rectangle "obstacle" and a green square "player" with a blue background.

--- VGA File/VGA_Bitgen.v
+++ VGA File/VGA_Bitgen.v
@@ -1,19 +1,40 @@
 module VGA_Bitgen (
-	input wire clock, bright, slowPulse
-	input wire [9:0] hCount, vCount, xLocation, yLocation,
-	input wire [7:0] pixelData,
+	input bright,
+	input [9:0] hCount,
+	input [9:0] vCount,
 	
-	output wire [7:0] rgb
+	input [9:0] player_x,
+	input [9:0] player_y,
 	
-	// clock, slowPulse, x and y locations are not needed
+	input [9:0] obs_x,
+	input [9:0] obs_y,
+	
+	output [3:0] red,
+	output [3:0] green,
+	output [3:0] blue
 );
 
-	// We can do three different techniques
-	// Bitmapped graphics -- not worth it, takes too much memory
-	// Character/glyph graphics
-	// Direct graphics
+	// Player size (width x height)
+	localparam PLAYER_W = 40;
+	localparam PLAYER_H = 40;
 	
-	// Colors we can get using 3-bits
-	// black, blue, green, cyan, red, magenta, yellow, white
+	// Obstacle size (width x height)
+	localparam OBS_W = 20;
+	localparam OBS_H = 80;
+	
+	// Check if inside player
+	wire player_on = (hCount >= player_x) && (hCount < player_x + PLAYER_W) &&
+						  (vCount >= player_y) && (vCount < player_y + PLAYER_H);
+						  
+	// Check if inside obstacle
+	wire obs_on = (hCount >= obs_x) && (hCount < obs_x + OBS_W) &&
+					  (vCount >= obs_y) && (vCount < obs_y + OBS_H);
+					  
+	// Output colors
+	assign {red, green, blue} =
+		!bright		? 12'h000 :		// outside visible area
+		player_on	? 12'h0F0 :		// green player
+		obs_on		? 12'hF00 :		// red obstacle
+						  12'h00F;		// blue background
 
 endmodule
