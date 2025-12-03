905082fc438e15315548f4a3e814ad894a7e4557
HoneyNat1 <23nacostahernandez@gmail.com>
2025-12-02 22:58:47 -0700

    Update topVGA.v
    moving logic added

--- VGA File/topVGA.v
+++ VGA File/topVGA.v
@@ -1,6 +1,10 @@
 module topVGA(
-    input  wire clk,
-    input  wire reset,
+    input wire clk,
+    input wire reset,
+	 
+	 input wire PS2_CLK,
+	 input wire PS2_DAT,
+	 
     output wire [3:0] r,
     output wire [3:0] g,
     output wire [3:0] b,
@@ -46,7 +50,8 @@ module topVGA(
 		 .bright(bright),
 		 .hCount(hCount),
 		 .vCount(vCount),
-	
+		 
+		 .key_status(key_status),
 		 .q_b(q_b),            // from BRAM port B
 		 .addr_b(addr_b),      // to BRAM port B
 	
@@ -55,6 +60,42 @@ module topVGA(
 		 .b(b)
 	);
 
-
+	wire [7:0] scan_code;
+	wire scan_ready;
+	
+	ps2_keyboard keyboard_hw (
+		 .clk(clk),
+		 .rst(!reset),
+		 .ps2_clk(PS2_CLK),
+		 .ps2_data(PS2_DAT),
+		 .data_out(scan_code),
+		 .data_ready(scan_ready)
+	);
+	
+	wire w_press, a_press, s_press, d_press;
+	wire space_press, r_press;
+	
+	key_decoder decoder(
+		 .clk(clk),
+		 .rst(!reset),
+		 .data_in(scan_code),
+		 .data_ready(scan_ready),
+		 .w_press(w_press),
+		 .a_press(a_press),
+		 .s_press(s_press),
+		 .d_press(d_press),
+		 .space_press(space_press),
+		 .r_press(r_press)
+	);
+	
+	wire [5:0] key_status;
+	
+	ps2_status status(
+		 .clk(clk),
+		 .rst(!reset),
+		 .scan_code(scan_code),
+		 .scan_ready(scan_ready),
+		 .key_status(key_status)
+	);
 
 endmodule
